e2d74ff7f6a955cc187773930ab0488eaa599359
Victor Cheng <victorcheng12345@gmail.com>
2022-02-09 19:15:03 -0800

    Call startService before bindService

--- android/src/main/kotlin/io/agora/agora_rtc_engine/AgoraRtcEnginePlugin.kt
+++ android/src/main/kotlin/io/agora/agora_rtc_engine/AgoraRtcEnginePlugin.kt
@@ -244,6 +244,7 @@ open class AgoraRtcEnginePlugin :
     println("reached bind service")
     val videoInputIntent = Intent(myContext, ExternalVideoInputService::class.java)
     mServiceConnection = VideoInputServiceConnection()
+    myContext.startService(videoInputIntent)
     myContext.bindService(videoInputIntent, mServiceConnection!!, BIND_AUTO_CREATE)
     println("finished bind service")
 
