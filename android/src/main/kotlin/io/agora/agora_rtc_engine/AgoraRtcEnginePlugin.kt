d9b6c9fd2bc65c976a2c4c7117ce90d6bfc7969c
Victor Cheng <victorcheng12345@gmail.com>
2022-02-10 19:00:05 -0800

    Use context over activity

--- android/src/main/kotlin/io/agora/agora_rtc_engine/AgoraRtcEnginePlugin.kt
+++ android/src/main/kotlin/io/agora/agora_rtc_engine/AgoraRtcEnginePlugin.kt
@@ -284,11 +284,11 @@ class StartScreenShareActivity : Activity() {
   var mService: IExternalVideoInputService? = null
   var mServiceConnection: VideoInputServiceConnection? = null
   var dataIntent: Intent? = null
-  var myActivity: Activity? = null
+  var myContext: Context? = null
 
   override fun onCreate(bundle: Bundle?) {
     super.onCreate(bundle)
-    myActivity = this
+    myContext = this
     println("StartScreenShareActivity created")
     val mpm = this.getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
     val captureIntent = mpm.createScreenCaptureIntent()
@@ -320,9 +320,9 @@ class StartScreenShareActivity : Activity() {
       dataIntent!!.putExtra(ExternalVideoInputManager.FLAG_FRAME_RATE, 15)
       AgoraRtcEnginePlugin().setVideoConfig(metrics.widthPixels, metrics.heightPixels)
 
-      val videoInputIntent = Intent(myActivity, ExternalVideoInputService::class.java)
+      val videoInputIntent = Intent(myContext, ExternalVideoInputService::class.java)
       mServiceConnection = VideoInputServiceConnection()
-      val didBind = myActivity?.bindService(videoInputIntent, mServiceConnection!!, BIND_AUTO_CREATE)
+      val didBind = myContext?.bindService(videoInputIntent, mServiceConnection!!, BIND_AUTO_CREATE)
       println("finished bind service as: $didBind")
     }
 //    finish()
