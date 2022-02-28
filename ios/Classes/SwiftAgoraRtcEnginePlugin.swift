66db0cf50f4a9b8d084fd6414cac3e6a53b36166
Victor Cheng <victorcheng12345@gmail.com>
2022-02-28 12:10:46 -0800

    Dont auto resize

--- ios/Classes/SwiftAgoraRtcEnginePlugin.swift
+++ ios/Classes/SwiftAgoraRtcEnginePlugin.swift
@@ -92,7 +92,7 @@ public class SwiftAgoraRtcEnginePlugin: NSObject, FlutterPlugin, FlutterStreamHa
         if #available(iOS 12.0, *) {
             let frame = CGRect(x: 0, y:0, width: 60, height: 60)
             let systemBroadcastPicker = RPSystemBroadcastPickerView(frame: frame)
-            systemBroadcastPicker.autoresizingMask = [.flexibleTopMargin, .flexibleRightMargin]
+//            systemBroadcastPicker.autoresizingMask = [.flexibleTopMargin, .flexibleRightMargin]
             if let url = Bundle.main.url(forResource: "AgoraScreenShareExtension", withExtension: "appex", subdirectory: "PlugIns") {
                 if let bundle = Bundle(url: url) {
                     systemBroadcastPicker.preferredExtension = bundle.bundleIdentifier
