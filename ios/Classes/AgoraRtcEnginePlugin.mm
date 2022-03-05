3ea2b7ec14f2172411fcde131a14322648d9cd3e
Victor Cheng <victorcheng12345@gmail.com>
2022-03-04 16:36:56 -0800

    Add startScreenShare method to launch broadcast button

--- ios/Classes/AgoraRtcEnginePlugin.mm
+++ ios/Classes/AgoraRtcEnginePlugin.mm
@@ -1,6 +1,7 @@
 #import "AgoraRtcEnginePlugin.h"
 #import <AgoraRtcWrapper/iris_rtc_engine.h>
 #import <AgoraRtcWrapper/iris_video_processor.h>
+#import <ReplayKit/ReplayKit.h>
 #import "CallApiMethodCallHandler.h"
 #import "FlutterIrisEventHandler.h"
 #import "AgoraRtcChannelPlugin.h"
@@ -79,8 +80,9 @@
         return;
     }
 #endif
-    
-    if ([@"createTextureRender" isEqualToString:call.method]) {
+    if ([@"startScreenShare" isEqualToString:call.method]) {
+        [self launchReplayKitBroadcast:call.arguments[@"extensionName"] result:result];
+    } else if ([@"createTextureRender" isEqualToString:call.method]) {
         int64_t textureId = [self.factory
             createTextureRenderer:[self videoFrameBufferManager]];
         result(@(textureId));
@@ -92,6 +94,44 @@
         [self getAssetAbsolutePath:call result:result];
     } else {
         [[self callApiMethodCallHandler] onMethodCall:call _:result];
+    }
+}
+
+- (void)launchReplayKitBroadcast:(NSString *)extensionName result:(FlutterResult)result {
+    if (@available(iOS 12.0, *)) {
+        RPSystemBroadcastPickerView *broadcastPickerView = [[RPSystemBroadcastPickerView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+        
+        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:extensionName ofType:@"appex" inDirectory:@"PlugIns"];
+        if (!bundlePath) {
+            NSString *nullBundlePathErrorMessage = [NSString stringWithFormat:@"Can not find path for bundle `%@.appex`", extensionName];
+            NSLog(@"%@", nullBundlePathErrorMessage);
+            result([FlutterError errorWithCode:@"NULL_BUNDLE_PATH" message:nullBundlePathErrorMessage details:nil]);
+            return;
+        }
+
+        NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+        if (!bundle) {
+            NSString *nullBundleErrorMessage = [NSString stringWithFormat:@"Can not find bundle at path: `%@`", bundlePath];
+            NSLog(@"%@", nullBundleErrorMessage);
+            result([FlutterError errorWithCode:@"NULL_BUNDLE" message:nullBundleErrorMessage details:nil]);
+            return;
+        }
+
+        broadcastPickerView.preferredExtension = bundle.bundleIdentifier;
+
+        // Traverse the subviews to find the button to skip the step of clicking the system view
+        for (UIView *subView in broadcastPickerView.subviews) {
+            if ([subView isMemberOfClass:[UIButton class]]) {
+                UIButton *button = (UIButton *)subView;
+                [button sendActionsForControlEvents:UIControlEventAllEvents];
+            }
+        }
+        result(@(YES));
+
+    } else {
+        NSString *notAvailiableMessage = @"RPSystemBroadcastPickerView is only available on iOS 12.0 or above";
+        NSLog(@"%@", notAvailiableMessage);
+        result([FlutterError errorWithCode:@"NOT_AVAILIABLE" message:notAvailiableMessage details:nil]);
     }
 }
 
