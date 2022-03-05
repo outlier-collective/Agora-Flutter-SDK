1a305b8cb16b82a970cdd96b73ebc3ff7fe2b142
Victor Cheng <victorcheng12345@gmail.com>
2022-03-04 16:55:23 -0800

    Comment out launch ReplayKit

--- ios/Classes/AgoraRtcEnginePlugin.mm
+++ ios/Classes/AgoraRtcEnginePlugin.mm
@@ -81,7 +81,7 @@
     }
 #endif
     if ([@"startScreenShare" isEqualToString:call.method]) {
-        [self launchReplayKitBroadcast:call.arguments[@"extensionName"] result:result];
+//        [self launchReplayKitBroadcast:call.arguments[@"extensionName"] result:result];
     } else if ([@"createTextureRender" isEqualToString:call.method]) {
         int64_t textureId = [self.factory
             createTextureRenderer:[self videoFrameBufferManager]];
@@ -97,43 +97,43 @@
     }
 }
 
-- (void)launchReplayKitBroadcast:(NSString *)extensionName result:(FlutterResult)result {
-    if (@available(iOS 12.0, *)) {
-        RPSystemBroadcastPickerView *broadcastPickerView = [[RPSystemBroadcastPickerView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
-        
-        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:extensionName ofType:@"appex" inDirectory:@"PlugIns"];
-        if (!bundlePath) {
-            NSString *nullBundlePathErrorMessage = [NSString stringWithFormat:@"Can not find path for bundle `%@.appex`", extensionName];
-            NSLog(@"%@", nullBundlePathErrorMessage);
-            result([FlutterError errorWithCode:@"NULL_BUNDLE_PATH" message:nullBundlePathErrorMessage details:nil]);
-            return;
-        }
-
-        NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
-        if (!bundle) {
-            NSString *nullBundleErrorMessage = [NSString stringWithFormat:@"Can not find bundle at path: `%@`", bundlePath];
-            NSLog(@"%@", nullBundleErrorMessage);
-            result([FlutterError errorWithCode:@"NULL_BUNDLE" message:nullBundleErrorMessage details:nil]);
-            return;
-        }
-
-        broadcastPickerView.preferredExtension = bundle.bundleIdentifier;
-
-        // Traverse the subviews to find the button to skip the step of clicking the system view
-        for (UIView *subView in broadcastPickerView.subviews) {
-            if ([subView isMemberOfClass:[UIButton class]]) {
-                UIButton *button = (UIButton *)subView;
-                [button sendActionsForControlEvents:UIControlEventAllEvents];
-            }
-        }
-        result(@(YES));
-
-    } else {
-        NSString *notAvailiableMessage = @"RPSystemBroadcastPickerView is only available on iOS 12.0 or above";
-        NSLog(@"%@", notAvailiableMessage);
-        result([FlutterError errorWithCode:@"NOT_AVAILIABLE" message:notAvailiableMessage details:nil]);
-    }
-}
+//- (void)launchReplayKitBroadcast:(NSString *)extensionName result:(FlutterResult)result {
+//    if (@available(iOS 12.0, *)) {
+//        RPSystemBroadcastPickerView *broadcastPickerView = [[RPSystemBroadcastPickerView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+//        
+//        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:extensionName ofType:@"appex" inDirectory:@"PlugIns"];
+//        if (!bundlePath) {
+//            NSString *nullBundlePathErrorMessage = [NSString stringWithFormat:@"Can not find path for bundle `%@.appex`", extensionName];
+//            NSLog(@"%@", nullBundlePathErrorMessage);
+//            result([FlutterError errorWithCode:@"NULL_BUNDLE_PATH" message:nullBundlePathErrorMessage details:nil]);
+//            return;
+//        }
+//
+//        NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+//        if (!bundle) {
+//            NSString *nullBundleErrorMessage = [NSString stringWithFormat:@"Can not find bundle at path: `%@`", bundlePath];
+//            NSLog(@"%@", nullBundleErrorMessage);
+//            result([FlutterError errorWithCode:@"NULL_BUNDLE" message:nullBundleErrorMessage details:nil]);
+//            return;
+//        }
+//
+//        broadcastPickerView.preferredExtension = bundle.bundleIdentifier;
+//
+//        // Traverse the subviews to find the button to skip the step of clicking the system view
+//        for (UIView *subView in broadcastPickerView.subviews) {
+//            if ([subView isMemberOfClass:[UIButton class]]) {
+//                UIButton *button = (UIButton *)subView;
+//                [button sendActionsForControlEvents:UIControlEventAllEvents];
+//            }
+//        }
+//        result(@(YES));
+//
+//    } else {
+//        NSString *notAvailiableMessage = @"RPSystemBroadcastPickerView is only available on iOS 12.0 or above";
+//        NSLog(@"%@", notAvailiableMessage);
+//        result([FlutterError errorWithCode:@"NOT_AVAILIABLE" message:notAvailiableMessage details:nil]);
+//    }
+//}
 
 - (void)getAssetAbsolutePath:(FlutterMethodCall *)call
                       result:(FlutterResult)result {
